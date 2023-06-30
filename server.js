c3a1290fa454dd1e4f465e189ae03cee2f8d55d4
daantamba <daantamba@gmail.com>
2023-06-30 01:24:12 -0500

    Update with deslizar2

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ const cors = require('cors');
 
 const secretKey = process.env.SECRET_KEY;
 
-app.set('port', process.env.PORT || 9600  )
+app.set('port', process.env.PORT || 8080  )
 const dbOptions ={
-    host: 'localhost',
+/*     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
-    database: 'homefitgo'
-/*     host: 'mysql-132581-0.cloudclusters.net',
-    port: 10078,
-    user: 'admin',
-    password: 'sBVQxf85',
     database: 'homefitgo' */
+    host: 'mysql-133782-0.cloudclusters.net',
+    port: 14681,
+    user: 'admin',
+    password: 'xu6IjIe2',
+    database: 'homefitgo'
 }
 
 // Agregar middleware para permitir CORS
